17c01873520927fd0b70811a4d9c596c618305b9
Xu Haiming <haiming12322@qq.com>
2016-08-02 00:04:15 +0800

    Initialize project

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import request from 'superagent';
+
+class TopicList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      topics: []
+    };
+  }
+
+  componentWillMount() {
+    request
+      .get('https://cnodejs.org/api/v1/topics')
+      .end((err, res) => {
+        this.setState({
+          topics: res.body.data
+        });
+      });
+  }
+
+  render() {
+    const topicList = this.state.topics.map(topic => {
+      return <div>{JSON.stringify(topic)}</div>;
+    });
+
+    return (
+      <div>
+        {topicList}
+      </div>
+    );
+  }
+}
+
+render(<TopicList />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'main.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: [ 'babel' ],
+        exclude: /node_modules/,
+        include: __dirname
+      }
+    ]
+  }
+};
