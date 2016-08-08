eb90ff0c8212508c6321da1718dbf053beddf969
Xu Haiming <haiming12322@qq.com>
2016-08-08 23:58:47 +0800

    Introduct react router

--- src/index.js
+++ src/index.js
@@ -4,20 +4,31 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import MainPage from './components/MainPage';
+import { Router, Route, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
+
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
+const RootContainer = ({ params }) => (
+  <MuiThemeProvider>
+    <MainPage activeTab={params.tabName || 'all'} />
+  </MuiThemeProvider>
+);
+
 const App = () => {
   const store = createStore(reducers);
+  const history = useRouterHistory(createHashHistory)({ queryKey: false });
 
   return (
     <Provider store={store}>
-      <MuiThemeProvider>
-        <MainPage />
-      </MuiThemeProvider>
+      <Router history={history}>
+        <Route path="/" component={RootContainer} />
+        <Route path="/:tabName" component={RootContainer} />
+      </Router>
     </Provider>
   );
 };
