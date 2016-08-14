17edab3985b18456d9006f2801f9cd7faada7f12
Xu Haiming <haiming12322@qq.com>
2016-08-14 16:54:17 +0800

    Add style for topic detail page

--- src/components/TopicItem.js
+++ src/components/TopicItem.js
@@ -37,4 +37,3 @@ export default connect(
   mapDispatchToProps
 )(TopicItem);
 
-
--- src/components/TopicPage.js
+++ src/components/TopicPage.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import CircularProgress from 'material-ui/CircularProgress';
+import { get } from '../api/client';
+import { Card, CardHeader, CardTitle, CardText } from 'material-ui/Card';
+
+const styles = {
+  progress: {
+    margin: '250px auto',
+    display: 'block'
+  }
+};
 
 class TopicPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
+    const topic = this.state.data;
+
+    if (!topic) {
+      get(this.props.location).then(data => this.setState({ data }));
+
+      return <CircularProgress style={styles.progress} size={2} />;
+    }
+
     return (
-      <div> hello world</div>
+      <Card>
+        <CardHeader
+          title={topic.title}
+          subtitle={topic.author.loginname}
+          avatar={topic.author.avatar_url}
+        />
+        <CardTitle title={topic.title} subtitle={topic.create_at} />
+        <CardText>
+          <div dangerouslySetInnerHTML={{ __html: topic.content }}></div>
+        </CardText>
+      </Card>
     );
   }
 }
 
-export default TopicPage;
+TopicPage.propTypes = {
+  location: React.PropTypes.string.isRequired
+};
+
+const mapStateToProps = state => ({
+  location: state.routing.locationBeforeTransitions.pathname
+});
+
+export default connect(
+  mapStateToProps
+)(TopicPage);
